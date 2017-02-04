f1d9e8255c2f171f18407664b4cc7eb231784ba5
Alex Wynter <awochna@email.arizona.edu>
2017-02-04 15:29:58 -0700

    Add documentation example for %Breaker.OpenCircuitError{}

--- lib/breaker/open_circuit_error.ex
+++ lib/breaker/open_circuit_error.ex
@@ -1,6 +1,12 @@
 defmodule Breaker.OpenCircuitError do
   @moduledoc """
   Defines the struct returned when a request is made, but the circuit is open.
+
+  ## Examples: ##
+
+      iex> %Breaker.OpenCircuitError{}
+      %Breaker.OpenCircuitError{message: "circuit is open"}
+
   """
 
   defstruct message: "circuit is open"
