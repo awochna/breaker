cd4e77b4f2128f9a6dca6796fc75b813745cd5c1
Alex Wynter <awochna@email.arizona.edu>
2017-02-04 14:23:28 -0700

    Add @moduledoc for OpenCircuitError

--- lib/breaker/open_circuit_error.ex
+++ lib/breaker/open_circuit_error.ex
@@ -1,3 +1,7 @@
 defmodule Breaker.OpenCircuitError do
+  @moduledoc """
+  Defines the struct returned when a request is made, but the circuit is open.
+  """
+
   defstruct message: "circuit is open"
 end
