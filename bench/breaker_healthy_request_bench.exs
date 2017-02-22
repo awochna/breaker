32295bf27ab08cf7922d5227f86259678a06e4b2
Alex Wynter <awochna@email.arizona.edu>
2017-02-22 13:20:04 -0700

    Add a 'get without breaker' bench for each.

--- bench/breaker_healthy_request_bench.exs
+++ bench/breaker_healthy_request_bench.exs
@@ -6,8 +6,13 @@ defmodule BreakerHealthyRequestBench do
     Breaker.start_link(%{url: "http://localhost:8080/"})
   end
 
-  bench "get request", [breaker: bench_context] do
+  bench "get request with breaker", [breaker: bench_context] do
     task = Breaker.get(breaker, "/status/200")
     Breaker.error?(Task.await(task))
+  end
+
+  bench "get request without breaker" do
+    response = HTTPotion.get("http://localhost:8080/status/200")
+    Breaker.error?(response)
   end
 end
--- bench/breaker_unhealthy_request_bench.exs
+++ bench/breaker_unhealthy_request_bench.exs
@@ -6,8 +6,13 @@ defmodule BreakerUnhealthyRequestBench do
     Breaker.start_link(%{url: "http://localhost:8080/"})
   end
 
-  bench "get request", [breaker: bench_context] do
+  bench "get request with breaker", [breaker: bench_context] do
     task = Breaker.get(breaker, "/status/500")
     Breaker.error?(Task.await(task))
+  end
+
+  bench "get request without breaker" do
+    response = HTTPotion.get("http://localhost:8080/status/500")
+    Breaker.error?(response)
   end
 end
